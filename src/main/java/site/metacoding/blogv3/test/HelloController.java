1fd84e76061b44ed581b3a20d2c4da621362c09a
ssar <ssar@nate.com>
2022-04-08 12:50:11 +0900

    ~~~ 네비게이션바 진행중

--- src/main/java/site/metacoding/blogv3/test/HelloController.java
+++ src/main/java/site/metacoding/blogv3/test/HelloController.java
@@ -1,9 +1,9 @@
 package site.metacoding.blogv3.test;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
 
     @GetMapping("/test")
