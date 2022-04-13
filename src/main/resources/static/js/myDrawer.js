dcd2f7ab0f4d5da8872f7e67a18cbd1ae8dd7f27
ssar <ssar@nate.com>
2022-04-13 17:46:19 +0900

    footer 생성 및 text.css 생성

---
+++ src/main/resources/static/js/myDrawer.js
@@ -0,0 +1,17 @@
+$('.drawer').drawer({
+    class: {
+        nav: 'drawer-nav',
+        toggle: 'drawer-toggle',
+        overlay: 'drawer-overlay',
+        open: 'drawer-open',
+        close: 'drawer-close',
+        dropdown: 'drawer-dropdown'
+    },
+    iscroll: {
+        // Configuring the iScroll
+        // https://github.com/cubiq/iscroll#configuring-the-iscroll
+        mouseWheel: true,
+        preventDefault: false
+    },
+    showOverlay: true
+});
