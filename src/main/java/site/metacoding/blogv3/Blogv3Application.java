28af50b9db128a7d5ada4567af5ea22e44fd7470
ssar <ssar@nate.com>
2022-04-18 09:30:08 +0900

    [기능정리함- readme 수정]

--- src/main/java/site/metacoding/blogv3/Blogv3Application.java
+++ src/main/java/site/metacoding/blogv3/Blogv3Application.java
@@ -2,11 +2,7 @@ package site.metacoding.blogv3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
-import site.metacoding.blogv3.domain.user.UserRepository;
-import site.metacoding.blogv3.web.UserController;
 
 @EnableJpaAuditing
 @SpringBootApplication
--- src/main/java/site/metacoding/blogv3/web/dto/user/JoinReqDto.java
+++ src/main/java/site/metacoding/blogv3/web/dto/user/JoinReqDto.java
@@ -15,9 +15,10 @@ import site.metacoding.blogv3.domain.user.User;
 @Data // Getter, Setter, toString
 public class JoinReqDto {
 
-    @Pattern(regexp = "[a-zA-Z1-9]{4,20}", message = "유저네임은 한글이 들어갈 수 없습니다.")
+    @Pattern(regexp = "[a-zA-Z0-9]{4,20}", message = "유저네임은 한글이 들어갈 수 없습니다.")
     @Size(min = 4, max = 20)
     @NotBlank
+
     private String username;
 
     @Size(min = 4, max = 20)
