38f2f3ae0aea8d5cb29b3e11ed7a45aefad83f1a
jinyongkwon <wlsdyd1178@naver.com>
2022-04-29 18:51:42 +0900

    [권진용] 게시글 상세보기 서비스 분리 완료

--- src/main/java/site/metacoding/blogv3/util/UtilPost.java
+++ src/main/java/site/metacoding/blogv3/util/UtilPost.java
@@ -1,11 +1,22 @@
 package site.metacoding.blogv3.util;
 
+import java.util.Optional;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import lombok.RequiredArgsConstructor;
+import site.metacoding.blogv3.domain.post.Post;
+import site.metacoding.blogv3.domain.post.PostRepository;
+import site.metacoding.blogv3.domain.user.User;
+import site.metacoding.blogv3.handler.ex.CustomException;
+
+@RequiredArgsConstructor
 public class UtilPost {
+
+    private final PostRepository postRepository;
 
     // 책임 : img 태그 제거
     public static String getContentWithoutImg(String content) {
@@ -20,4 +31,5 @@ public class UtilPost {
 
         return doc.select("body").text();
     }
+
 }
