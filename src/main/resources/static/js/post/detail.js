72ada5814d35a17319ace27910e12c40022f1c04
jaewon2336 <58693821+jaewon2336@users.noreply.github.com>
2022-05-17 17:12:43 +0900

    [정재원] 좋아요, 좋아요취소 fetch 구현 완료 (#6)
    * 좋아요, 좋아요취소 fetch 구현 완료
    
    * [정재원] 좋아요, 좋아요취소 fetch 구현 완료

--- src/main/resources/static/js/post/detail.js
+++ src/main/resources/static/js/post/detail.js
@@ -7,20 +7,50 @@ function postLikeClick(id) {
     }
 }
 
-function postLike(id) {
+async function postLike(id) {
     // fetch();
-    $(`#heart-${id}`).addClass("my_fake_like");
-    $(`#heart-${id}`).removeClass("my_fake_un_like");
-    $(`#heart-${id}`).removeClass("far");
-    $(`#heart-${id}`).addClass("fa-solid");
+    let response = await fetch(`/s/api/post/${id}/love`, {
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8'
+        },
+        method: 'POST'
+    });
+
+    let responseParse = await response.json();
+
+
+    if (response.status == 201) {
+        $(`#heart-${id}`).addClass("my_fake_like");
+        $(`#heart-${id}`).removeClass("my_fake_un_like");
+        $(`#heart-${id}`).removeClass("far");
+        $(`#heart-${id}`).addClass("fa-solid");
+
+        $("#loveId").val(responseParse.loveId);
+        console.log(responseParse);
+    } else {
+        alert("통신실패");
+    }
 }
 
-function postUnLike(id) {
+async function postUnLike(id) {
+    let loveId = $("#loveId").val();
+    console.log(loveId);
     // fetch();
-    $(`#heart-${id}`).addClass("my_fake_un_like");
-    $(`#heart-${id}`).removeClass("my_fake_like");
-    $(`#heart-${id}`).removeClass("fa-solid");
-    $(`#heart-${id}`).addClass("far");
+    let response = await fetch(`/s/api/post/${id}/love/${loveId}`, {
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8'
+        },
+        method: 'DELETE'
+    });
+
+    if (response.status == 200) {
+        $(`#heart-${id}`).addClass("my_fake_un_like");
+        $(`#heart-${id}`).removeClass("my_fake_like");
+        $(`#heart-${id}`).removeClass("fa-solid");
+        $(`#heart-${id}`).addClass("far");
+    } else {
+        alert("통신실패");
+    }
 }
 
 // 게시글 삭제, 권한체크 후 삭제 /s/api/post/postId
